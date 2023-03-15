3b1e78c7c3c2404616bda7a796728b0cb3e4ce91
dom <davide.domini2@unibo.it>
2023-03-15 11:45:52 +0100

    feat: add state

---
+++ src/main/scala/experiments/CohesionCollisionState.scala
@@ -0,0 +1,14 @@
+package experiments
+
+import it.unibo.scarlib.core.model.State
+
+class CohesionCollisionState(positions: List[(Double, Double)], agentId: Int) extends State {
+  override def elements(): Int = 5 * 2
+
+  override def toSeq(): Seq[Double] = {
+    val fill = List.fill(elements())(0.0)
+    (positions.flatMap { case (l, r) => List(l, r) } ++ fill).take(elements())
+  }
+
+  override def isEmpty(): Boolean = false
+}
