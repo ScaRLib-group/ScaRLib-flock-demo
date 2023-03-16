b57feb43b020d6c09f9d09b41c7484a1b615d0ed
dom <davide.domini2@unibo.it>
2023-03-16 14:33:50 +0100

    feat: implement first dqn factory

--- src/main/scala/experiments/NNFactory.scala
+++ src/main/scala/experiments/NNFactory.scala
@@ -1,3 +1,10 @@
-package experiments class NNFactory {
+package experiments
+
+import it.unibo.scarlib.core.neuralnetwork.{DQNAbstractFactory, SimpleSequentialDQN}
+import me.shadaj.scalapy.py
+
+class NNFactory extends DQNAbstractFactory[py.Dynamic] {
+
+  override def createNN(): py.Dynamic = SimpleSequentialDQN(0,0,0)
 
 }
