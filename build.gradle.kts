160299ac2ff9f08bcd6c964271a598932e4e8e31
dom <davide.domini2@unibo.it>
2023-03-16 14:40:32 +0100

    chore(deps): add alchemist protelis

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,10 @@ dependencies {
     implementation("org.scala-lang:scala-library:2.13.10")
     implementation("io.github.davidedomini:scarlib-core:$scarlibVersion")
     implementation("io.github.davidedomini:alchemist-scafi:$scarlibVersion")
-    implementation("it.unibo.alchemist:alchemist:25.7.1")
-    implementation("it.unibo.alchemist:alchemist-incarnation-scafi:25.7.1")
+    implementation("it.unibo.alchemist:alchemist:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-incarnation-scafi:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-incarnation-protelis:25.14.6")
+    implementation("it.unibo.alchemist:alchemist-swingui:25.7.1")
     implementation("dev.scalapy:scalapy-core_2.13:0.5.3")
     //implementation("io.github.davidedomini:dsl-core:1.5.0")
 
