6cae87aa52cd852a1d2fea7e00980e04770592dc
dom <davide.domini2@unibo.it>
2023-03-16 14:33:31 +0100

    chore(deps): update scarlib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,13 +28,16 @@ sourceSets {
     }
 }
 
+val scarlibVersion = "1.6.2"
+
 dependencies {
     //implementation("org.scala-lang:scala3-library_3:3.2.2")
     implementation("org.scala-lang:scala-library:2.13.10")
-    implementation("io.github.davidedomini:scarlib-core:1.6.0")
-    implementation("io.github.davidedomini:alchemist-scafi:1.6.0")
+    implementation("io.github.davidedomini:scarlib-core:$scarlibVersion")
+    implementation("io.github.davidedomini:alchemist-scafi:$scarlibVersion")
     implementation("it.unibo.alchemist:alchemist:25.7.1")
     implementation("it.unibo.alchemist:alchemist-incarnation-scafi:25.7.1")
+    implementation("dev.scalapy:scalapy-core_2.13:0.5.3")
     //implementation("io.github.davidedomini:dsl-core:1.5.0")
 
 }
