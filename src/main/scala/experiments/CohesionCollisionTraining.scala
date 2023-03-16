29b130979738db9278bceb48e54779e8d17efeb2
dom <davide.domini2@unibo.it>
2023-03-16 14:59:50 +0100

    feat: implement training specification

---
+++ src/main/scala/experiments/CohesionCollisionTraining.scala
@@ -0,0 +1,39 @@
+package experiments
+
+import it.unibo.alchemist.AlchemistEnvironment
+import it.unibo.alchemist.loader.m2m.{JVMConstructor, SimulationModel}
+import it.unibo.scarlib.core.deepRL.{CTDESystem, IndependentAgent}
+import it.unibo.scarlib.core.model.{Action, LearningConfiguration, ReplayBuffer, State}
+import org.slf4j.LoggerFactory
+import ch.qos.logback.classic.Level
+
+object CohesionCollisionTraining {
+
+  private val rewardFunction = new CohesionCollisionRF()
+  LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
+  LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
+
+  private val actionSpace = CohesionCollisionActions.toSeq()
+
+  private val env = new AlchemistEnvironment(
+    rewardFunction,
+    actionSpace,
+    "./src/main/scala/experiment/CohesionCollisionSimulation.yaml",
+    randomSeed = Some(42)
+  )
+
+  private val datasetSize = 10000
+
+  private val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
+
+  private var agents: Seq[IndependentAgent] = Seq.empty
+  for (n <- 0 to 49)
+    agents = agents :+ new IndependentAgent(n, env, actionSpace, dataset)
+
+  private val learningConfiguration = new LearningConfiguration(dqnFactory = new NNFactory)
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  new CTDESystem(agents, env, dataset, actionSpace, learningConfiguration).learn(1000, 100)
+
+}
