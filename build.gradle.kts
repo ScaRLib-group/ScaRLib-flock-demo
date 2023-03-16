b10e34d67a522d238b2eef386bef22922ee92078
dom <davide.domini2@unibo.it>
2023-03-16 15:00:08 +0100

    chore(deps): add log deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ sourceSets {
 val scarlibVersion = "1.6.2"
 
 dependencies {
-    //implementation("org.scala-lang:scala3-library_3:3.2.2")
     implementation("org.scala-lang:scala-library:2.13.10")
     implementation("io.github.davidedomini:scarlib-core:$scarlibVersion")
     implementation("io.github.davidedomini:alchemist-scafi:$scarlibVersion")
@@ -40,6 +39,7 @@ dependencies {
     implementation("it.unibo.alchemist:alchemist-incarnation-protelis:25.14.6")
     implementation("it.unibo.alchemist:alchemist-swingui:25.7.1")
     implementation("dev.scalapy:scalapy-core_2.13:0.5.3")
-    //implementation("io.github.davidedomini:dsl-core:1.5.0")
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
 
 }
