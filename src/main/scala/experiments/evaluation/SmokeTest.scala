11edc046a8b06dcac552f44b063e93cada0afefc
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-04-15 11:17:02 +0200

    chore update smoke test

--- src/main/scala/experiments/evaluation/SmokeTest.scala
+++ src/main/scala/experiments/evaluation/SmokeTest.scala
@@ -16,29 +16,25 @@ object SmokeTest extends App {
   LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
   LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
 
-  def runEvaluationWith(count: Int, simulations: Int = 16): Unit = {
-    (0 until simulations).foreach { case seed =>
-      val env = new AlchemistEnvironment(
-        envDefinition = s"./src/main/scala/experiments/evaluation/CohesionAndCollisionEval-${count}.yaml",
-        rewardFunction = rewardFunction,
-        actionSpace = CohesionCollisionActions.toSeq(),
-        randomSeed = Some(seed),
-        outputStrategy = new ShowEach(1)
-      )
-      val datasetSize = 10000
-      println(s"------ Simulation ${seed} with ${env.currentNodeCount} agents --------")
-      val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
-      var agents: Seq[IndependentAgent] = Seq.empty
-      for (n <- 0 until env.currentNodeCount)
-        agents = agents :+ new IndependentAgent(n, env, CohesionCollisionActions.toSeq(), dataset)
+  val env = new AlchemistEnvironment(
+    envDefinition = s"./src/main/scala/experiments/evaluation/smoke",
+    rewardFunction = rewardFunction,
+    actionSpace = CohesionCollisionActions.toSeq(),
+    randomSeed = Some(42),
+    outputStrategy = new ShowEach(1)
+  )
+  val datasetSize = 10000
+  println(s"------ Simulation ${42} with ${env.currentNodeCount} agents --------")
+  val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
+  var agents: Seq[IndependentAgent] = Seq.empty
+  for (n <- 0 until env.currentNodeCount)
+    agents = agents :+ new IndependentAgent(n, env, CohesionCollisionActions.toSeq(), dataset)
 
-      val where = s"./networks/network"
-      val learningConfiguration = new LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = "networks/network")
+  val where = s"./networks/network"
+  val learningConfiguration = new LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = "networks/network")
 
-      new CTDESystem(agents, env, dataset, CohesionCollisionActions.toSeq(), learningConfiguration)
-        .runTest(ExperimentInfo.episodeLength, PolicyNN.apply(where, StateInfo.encoding * StateInfo.neighborhood, ExperimentInfo.hiddenSize))
-    }
-  }
-  runEvaluationWith(50, 1)
+  new CTDESystem(agents, env, dataset, CohesionCollisionActions.toSeq(), learningConfiguration)
+    .runTest(ExperimentInfo.episodeLength, PolicyNN.apply(where, StateInfo.encoding * StateInfo.neighborhood, ExperimentInfo.hiddenSize))
+
   System.exit(0)
 }
