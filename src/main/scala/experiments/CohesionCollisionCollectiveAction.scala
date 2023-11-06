6121c7581476fc58933c8d7c9ccea94b963d8a02
dom <davide.domini2@unibo.it>
2023-11-06 15:14:12 +0100

    feat!: update to scarlib 2.0.0

---
+++ src/main/scala/experiments/CohesionCollisionCollectiveAction.scala
@@ -0,0 +1,18 @@
+package experiments
+
+import it.unibo.alchemist.CollectiveAction
+import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
+import it.unibo.alchemist.model.interfaces.{Environment, Node, Position, TimeDistribution}
+
+class CohesionCollisionCollectiveAction[T, P <: Position[P]](
+    environment: Environment[T, P],
+    distribution: TimeDistribution[T])
+  extends CollectiveAction(environment, distribution) {
+
+  override protected def nodeAction(node: Node[T]): Unit = {
+    val destination = node.getConcentration(new SimpleMolecule("destination"))
+    if(destination != null){
+      environment.moveNodeToPosition(node, destination.asInstanceOf[P])
+    }
+  }
+}
--- src/main/scala/experiments/CohesionCollisionState.scala
+++ src/main/scala/experiments/CohesionCollisionState.scala
@@ -2,13 +2,22 @@ package experiments
 
 import it.unibo.scarlib.core.model.State
 import StateInfo._
-case class CohesionCollisionState(positions: List[(Double, Double)], agentId: Int) extends State {
-  override def elements(): Int = neighborhood * encoding
+import it.unibo.scarlib.core.neuralnetwork.NeuralNetworkEncoding
 
-  override def toSeq(): Seq[Double] = {
-    val fill = List.fill(elements())(0.0)
-    (positions.flatMap { case (l, r) => List(l, r) } ++ fill).take(elements())
-  }
+case class CohesionCollisionState(positions: List[(Double, Double)], agentId: Int) extends State {
 
   override def isEmpty(): Boolean = false
+}
+
+object CohesionCollisionState {
+  implicit val encoding: NeuralNetworkEncoding[State]
+    = new NeuralNetworkEncoding[State] {
+      override def elements(): Int = neighborhood * encoding
+
+      override def toSeq(element: State): Seq[Double] = {
+        val elem = element.asInstanceOf[CohesionCollisionState]
+        val fill = List.fill(elements())(0.0)
+        (elem.positions.flatMap { case (l, r) => List(l, r) } ++ fill).take(elements())
+      }
+    }
 }
