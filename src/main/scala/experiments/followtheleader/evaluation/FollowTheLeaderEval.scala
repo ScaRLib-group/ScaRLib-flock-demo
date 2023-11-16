daa9a238fb369b4d11249ed29a7ac02915d52b70
dom <davide.domini2@unibo.it>
2023-11-16 15:01:55 +0100

    feat: implement follow the leader experiment with dsl

--- src/main/scala/experiments/followtheleader/evaluation/FollowTheLeaderEval.scala
+++ src/main/scala/experiments/followtheleader/evaluation/FollowTheLeaderEval.scala
@@ -1,55 +1,59 @@
 package experiments.followtheleader.evaluation
 
 import it.unibo.alchemist.loader.m2m.{JVMConstructor, SimulationModel}
-import experiments.followtheleader.{ExperimentInfo, FollowTheLeaderActions, FollowTheLeaderNNFactory, FollowTheLeaderRF, FollowTheLeaderState, StateInfo}
+import experiments.followtheleader.{ExperimentInfo, FollowTheLeaderActions, FollowTheLeaderNNFactory, FollowTheLeaderRF, StateInfo}
 import it.unibo.alchemist.{AlchemistEnvironment, NoOutput, ShowEach}
 import org.slf4j.LoggerFactory
 import ch.qos.logback.classic.Level
-import it.unibo.scarlib.core.system.{CTDEAgent, CTDESystem}
-import it.unibo.scarlib.core.model.{Action, LearningConfiguration, ReplayBuffer, State}
+import it.unibo.scarlib.core.model.{Action, Environment, LearningConfiguration, ReplayBuffer, State}
 import it.unibo.scarlib.core.neuralnetwork.NeuralNetworkSnapshot
+import it.unibo.scarlib.dsl.DSL._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import experiments.followtheleader.FollowTheLeaderState.encoding
 
+
 object FollowTheLeaderEval extends App {
-  /*val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
+
+  val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
   val show = argsMap.get(0) match {
     case None => NoOutput
     case Some(steps) => new ShowEach(steps.toInt)
   }
 
-  private val rewardFunction = new FollowTheLeaderRF()
   LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
   LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
 
   def runEvaluationWith(count: Int, simulations: Int = 16): Unit = {
     (0 until simulations).foreach { case seed =>
-      val actionSpace = FollowTheLeaderActions.all()
-      val env = new AlchemistEnvironment(
-        envDefinition = s"./src/main/scala/experiments/FollowTheLeader/evaluation/FollowTheLeaderEval-${count}.yaml",
-        rewardFunction = rewardFunction,
-        actionSpace = actionSpace,
-        randomSeed = Some(seed),
-        outputStrategy = show
-      )
-      val datasetSize = 10000
-      println(s"------ Simulation ${seed} with ${env.currentNodeCount} agents --------")
-      val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
-      var agents: Seq[CTDEAgent] = Seq.empty
-      for (n <- 0 until env.currentNodeCount)
-        agents = agents :+ new CTDEAgent(n, env, actionSpace, dataset)
 
-      val where = s"./networks/network-followtheleader"
-      val learningConfiguration = new LearningConfiguration(dqnFactory = new FollowTheLeaderNNFactory, snapshotPath = "networks/network")
+      println(s"------ Simulation ${seed} with ${count} agents --------")
 
-      new CTDESystem(agents, env, dataset, actionSpace, learningConfiguration)
-        .runTest(ExperimentInfo.episodeLength, NeuralNetworkSnapshot(where, (StateInfo.encoding * StateInfo.neighborhood) + 2, ExperimentInfo.hiddenSize))
+      implicit val configuration: Environment => Unit = (e: Environment) => {
+        val env = e.asInstanceOf[AlchemistEnvironment]
+        env.setOutputStrategy(show)
+        env.setRandomSeed(Some(42))
+        env.setEnvironmentDefinition(s"./src/main/scala/experiments/followtheleader/evaluation/CohesionAndCollisionEval-${count}.yaml")
+      }
+
+      val where = s"./networks/network"
+
+      val system = CTDELearningSystem {
+        rewardFunction { new FollowTheLeaderRF() }
+        actionSpace { FollowTheLeaderActions.all() }
+        dataset { ReplayBuffer[State, Action](10000) }
+        agents { count }
+        learningConfiguration { LearningConfiguration(dqnFactory = new FollowTheLeaderNNFactory, snapshotPath = where) }
+        environment { "it.unibo.alchemist.AlchemistEnvironment" }
+      }
+      system
+        .runTest(ExperimentInfo.episodeLength, NeuralNetworkSnapshot(where, StateInfo.encoding * StateInfo.neighborhood, ExperimentInfo.hiddenSize))
     }
   }
 
   runEvaluationWith(50)
   runEvaluationWith(100)
   runEvaluationWith(200)
-  System.exit(0)*/
+
+  System.exit(0)
 }
--- src/main/scala/experiments/followtheleader/training/FollowTheLeaderTraining.scala
+++ src/main/scala/experiments/followtheleader/training/FollowTheLeaderTraining.scala
@@ -4,46 +4,43 @@ import it.unibo.alchemist.loader.m2m.{JVMConstructor, SimulationModel}
 import it.unibo.alchemist.{AlchemistEnvironment, NoOutput, ShowEach}
 import org.slf4j.LoggerFactory
 import ch.qos.logback.classic.Level
-import experiments.followtheleader.{ExperimentInfo, FollowTheLeaderActions, FollowTheLeaderNNFactory, FollowTheLeaderRF, FollowTheLeaderState}
-import it.unibo.scarlib.core.system.{CTDEAgent, CTDESystem}
+import experiments.followtheleader.{ExperimentInfo, FollowTheLeaderActions, FollowTheLeaderNNFactory, FollowTheLeaderRF}
 import it.unibo.scarlib.core.model.{Action, LearningConfiguration, ReplayBuffer, State}
+import it.unibo.scarlib.core.model.Environment
+import it.unibo.scarlib.dsl.DSL._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import experiments.followtheleader.FollowTheLeaderState.encoding
 
 
 object FollowTheLeaderTraining extends App {
-  /*val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
+
+  val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
   val show = argsMap.get(0) match {
     case None => NoOutput
     case Some(steps) => new ShowEach(steps.toInt)
   }
 
-  private val rewardFunction = new FollowTheLeaderRF()
   LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
   LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
 
-  private val actionSpace = FollowTheLeaderActions.all()
+  private implicit val config: Environment => Unit = (e: Environment) => {
+    val env = e.asInstanceOf[AlchemistEnvironment]
+    env.setOutputStrategy(show)
+    env.setRandomSeed(Some(42))
+    env.setEnvironmentDefinition("./src/main/scala/experiments/followtheleader/training/CohesionCollisionSimulation.yaml")
+  }
 
-  private val env = new AlchemistEnvironment(
-    rewardFunction,
-    actionSpace,
-    "./src/main/scala/experiments/followtheleader/FollowTheLeaderSimulation.yaml",
-    randomSeed = Some(42),
-    outputStrategy = show
-  )
+  val system = CTDELearningSystem {
+    rewardFunction { new FollowTheLeaderRF() }
+    actionSpace { FollowTheLeaderActions.all() }
+    dataset { ReplayBuffer[State, Action](10000) }
+    agents { 50 }
+    learningConfiguration { LearningConfiguration(dqnFactory = new FollowTheLeaderNNFactory, snapshotPath = "networks/network") }
+    environment { "it.unibo.alchemist.AlchemistEnvironment" }
+  }
 
-  private val datasetSize = 10000
+  system.learn(ExperimentInfo.episodes, ExperimentInfo.episodeLength)
 
-  private val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
-
-  private var agents: Seq[CTDEAgent] = Seq.empty
-
-  for (n <- 0 until env.currentNodeCount)
-    agents = agents :+ new CTDEAgent(n, env, actionSpace, dataset)
-
-  private val learningConfiguration = new LearningConfiguration(dqnFactory = new FollowTheLeaderNNFactory(), snapshotPath = "networks/network")
-
-  new CTDESystem(agents, env, dataset, actionSpace, learningConfiguration).learn(ExperimentInfo.episodes, ExperimentInfo.episodeLength)
-  System.exit(0)*/
+  System.exit(0)
 }
