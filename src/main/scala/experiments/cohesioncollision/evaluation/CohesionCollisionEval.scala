7ec3d5ed0dc2c40a3674cc16b880039b64d7d244
dom <davide.domini2@unibo.it>
2023-11-16 11:37:48 +0100

    feat: implement cohesion collision training and evaluation using dsl

--- src/main/scala/experiments/cohesioncollision/evaluation/CohesionCollisionEval.scala
+++ src/main/scala/experiments/cohesioncollision/evaluation/CohesionCollisionEval.scala
@@ -1,51 +1,57 @@
 package experiments.cohesioncollision.evaluation
 
 import ch.qos.logback.classic.Level
-import experiments.cohesioncollision.{CohesionCollisionActions, CohesionCollisionRF, CohesionCollisionState, ExperimentInfo, NNFactory, StateInfo}
+import experiments.cohesioncollision.{CohesionCollisionActions, CohesionCollisionRF, ExperimentInfo, NNFactory, StateInfo}
 import it.unibo.alchemist.{AlchemistEnvironment, NoOutput, ShowEach}
 import it.unibo.alchemist.loader.m2m.{JVMConstructor, SimulationModel}
-import it.unibo.scarlib.core.system.{CTDEAgent, CTDESystem}
 import it.unibo.scarlib.core.model._
 import org.slf4j.LoggerFactory
+import it.unibo.scarlib.dsl.DSL._
+import it.unibo.scarlib.core.model.Environment
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import experiments.cohesioncollision.CohesionCollisionState.encoding
 import it.unibo.scarlib.core.neuralnetwork.NeuralNetworkSnapshot
 
 object CohesionCollisionEval extends App {
+
   val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
   val show = argsMap.get(0) match {
     case None => NoOutput
     case Some(steps) => new ShowEach(steps.toInt)
   }
-  println(show)
-  private val rewardFunction = new CohesionCollisionRF()
+
   LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
   LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
 
   def runEvaluationWith(count: Int, simulations: Int = 16): Unit = {
     (0 until simulations).foreach { case seed =>
-      val env = new AlchemistEnvironment(
-        envDefinition = s"./src/main/scala/experiments/evaluation/CohesionAndCollisionEval-${count}.yaml",
-        rewardFunction = rewardFunction,
-        actionSpace = CohesionCollisionActions.toSeq(),
-        randomSeed = Some(seed),
-        outputStrategy = show
-      )
-      val datasetSize = 10000
-      println(s"------ Simulation ${seed} with ${env.currentNodeCount} agents --------")
-      val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
-      var agents: Seq[CTDEAgent] = Seq.empty
-      for (n <- 0 until env.currentNodeCount)
-        agents = agents :+ new CTDEAgent(n, env, CohesionCollisionActions.toSeq(), dataset)
+
+      println(s"------ Simulation ${seed} with ${count} agents --------")
+
+      implicit val configuration: Environment => Unit = (e: Environment) => {
+        val env = e.asInstanceOf[AlchemistEnvironment]
+        env.setOutputStrategy(show)
+        env.setRandomSeed(Some(42))
+        env.setEnvironmentDefinition(s"./src/main/scala/experiments/evaluation/CohesionAndCollisionEval-${count}.yaml")
+      }
 
       val where = s"./networks/network"
-      val learningConfiguration = new LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = "networks/network")
 
-      new CTDESystem(agents, env, dataset, CohesionCollisionActions.toSeq(), learningConfiguration)
+      val system = CTDELearningSystem {
+        rewardFunction { new CohesionCollisionRF() }
+        actionSpace { CohesionCollisionActions.toSeq() }
+        dataset { ReplayBuffer[State, Action](10000) }
+        agents { count }
+        learningConfiguration { LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = where) }
+        environment { "it.unibo.alchemist.AlchemistEnvironment" }
+      }
+
+      system
         .runTest(ExperimentInfo.episodeLength, NeuralNetworkSnapshot(where, StateInfo.encoding * StateInfo.neighborhood, ExperimentInfo.hiddenSize))
     }
   }
+
   runEvaluationWith(50)
   runEvaluationWith(100)
   runEvaluationWith(200)
--- src/main/scala/experiments/cohesioncollision/training/CohesionCollisionTraining.scala
+++ src/main/scala/experiments/cohesioncollision/training/CohesionCollisionTraining.scala
@@ -1,46 +1,45 @@
 package experiments.cohesioncollision.training
 
 import ch.qos.logback.classic.Level
-import experiments.cohesioncollision.{CohesionCollisionActions, CohesionCollisionRF, CohesionCollisionState, ExperimentInfo, NNFactory}
+import experiments.cohesioncollision.{CohesionCollisionActions, CohesionCollisionRF, ExperimentInfo, NNFactory}
 import it.unibo.alchemist.{AlchemistEnvironment, NoOutput, ShowEach}
 import it.unibo.alchemist.loader.m2m.{JVMConstructor, SimulationModel}
-import it.unibo.scarlib.core.system.{CTDEAgent, CTDESystem}
+import it.unibo.scarlib.core.model.Environment
 import it.unibo.scarlib.core.model.{Action, LearningConfiguration, ReplayBuffer, State}
 import org.slf4j.LoggerFactory
+import it.unibo.scarlib.dsl.DSL._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import experiments.cohesioncollision.CohesionCollisionState.encoding
 
 object CohesionCollisionTraining extends App {
+
   val argsMap = args.zipWithIndex.map { case (arg, i) => (i, arg) }.toMap
   val show = argsMap.get(0) match {
     case None => NoOutput
     case Some(steps) => new ShowEach(steps.toInt)
   }
-  private val rewardFunction = new CohesionCollisionRF()
+
   LoggerFactory.getLogger(classOf[SimulationModel]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
   LoggerFactory.getLogger(classOf[JVMConstructor]).asInstanceOf[ch.qos.logback.classic.Logger].setLevel(Level.OFF)
 
-  private val actionSpace = CohesionCollisionActions.toSeq()
+  private implicit val configuration: Environment => Unit = (e: Environment) => {
+    val env = e.asInstanceOf[AlchemistEnvironment]
+    env.setOutputStrategy(show)
+    env.setRandomSeed(Some(42))
+    env.setEnvironmentDefinition("./src/main/scala/experiments/training/CohesionCollisionSimulation.yaml")
+  }
 
-  private val env = new AlchemistEnvironment(
-    rewardFunction,
-    actionSpace,
-    "./src/main/scala/experiments/training/CohesionCollisionSimulation.yaml",
-    randomSeed = Some(42),
-    outputStrategy = show
-  )
+  val system = CTDELearningSystem {
+    rewardFunction { new CohesionCollisionRF() }
+    actionSpace { CohesionCollisionActions.toSeq() }
+    dataset { ReplayBuffer[State, Action](10000) }
+    agents { 50 }
+    learningConfiguration { LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = "networks/network") }
+    environment { "it.unibo.alchemist.AlchemistEnvironment" }
+  }
 
-  private val datasetSize = 10000
+  system.learn(ExperimentInfo.episodes, ExperimentInfo.episodeLength)
 
-  private val dataset: ReplayBuffer[State, Action] = ReplayBuffer[State, Action](datasetSize)
-
-  private var agents: Seq[CTDEAgent] = Seq.empty
-  for (n <- 0 until env.currentNodeCount)
-    agents = agents :+ new CTDEAgent(n, env, actionSpace, dataset)
-
-  private val learningConfiguration = new LearningConfiguration(dqnFactory = new NNFactory, snapshotPath = "networks/network")
-
-  new CTDESystem(agents, env, dataset, actionSpace, learningConfiguration).learn(ExperimentInfo.episodes, ExperimentInfo.episodeLength)
   System.exit(0)
 }
